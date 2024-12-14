6e24ffba236ee85534fbc01f25cbe38ad6d97a38
sahil saini <csesai07@gmail.com>
2024-12-14 10:02:53 +0530

    show user details and update user details work done

---
+++ .history/client/src/components/profile/ProfileInfo_20241211183430.jsx
@@ -0,0 +1,207 @@
+import React, { useState } from "react";
+import "./profileinfo.css";
+import { useFormik } from "formik";
+import { Toaster } from "react-hot-toast";
+import { updateProfileInfo } from "../../helper/helper";
+import { useUser } from "../../redux/context/UserContext";
+
+
+function ProfileInfo() {
+  const { user } = useUser();
+  const [firstNameEditMode, setFirstNameEditMode] = useState(false);
+  const [lastNameEditMode, setLastNameEditMode] = useState(false);
+  const [emailEditMode, setEmailEditMode] = useState(false);
+  const [phoneNoEditMode, setPhoneNoEditMode] = useState(false);
+
+  const { handleSubmit, getFieldProps, touched, errors } = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phoneNo: "",
+    },
+    // validate: (values) => {
+    //   const errors = {};
+
+    //   if (firstNameEditMode && !values.firstName.trim()) {
+    //     errors.firstName = "First name is required";
+    //   }
+
+    //   else if (lastNameEditMode && !values.lastName.trim()) {
+    //     errors.lastName = "Last name is required";
+    //   }
+
+    //   if (emailEditMode && !values.email.trim()) {
+    //     errors.email = "Email is required";
+    //   } else if (
+    //     emailEditMode &&
+    //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+    //   ) {
+    //     errors.email = "Invalid email address";
+    //   }
+
+    //   if (phoneNoEditMode && !values.phoneNo.trim()) {
+    //     errors.phoneNo = "Phone number is required";
+    //   }
+
+    //   return errors;
+    // },
+    validateOnBlur: true,
+    validateOnChange: true,
+    onSubmit: async (values) => {
+      try {
+        console.log(values);
+        const response = await updateProfileInfo({ credential: values });
+        console.log(response);
+        // You may update UI or show a success message here
+      } catch (error) {
+        console.error("Error in onSubmit: ", error);
+      }
+    },
+  });
+
+  const toggleEditMode = (editModeSetter) => {
+    editModeSetter((prevEditMode) => !prevEditMode);
+  };
+
+  return (
+    <div className="profile-info-wrapper">
+      <Toaster position="bottom-right"></Toaster>
+      <div className="profile-info-container">
+        <div className="info-head-10ih">
+          <span className="_10ihqw">Personal Information</span>
+          <span
+            className="_10ihec"
+            onClick={() => toggleEditMode(setFirstNameEditMode)}
+          >
+            edit
+          </span>
+        </div>
+        <form className="info-form-10ihfff" onSubmit={handleSubmit}>
+          <div>
+            {" "}
+            <div className="form-10ihfff-fl">
+              <div className="form-fl-10ihzl">
+                <label htmlFor="">First Name</label>
+                <input
+                  type="text"
+                  name="firstName"
+                  id="_10ihfn"
+                  {...getFieldProps("firstName")}
+                  placeholder={user.firstname}
+                  disabled={!firstNameEditMode}
+                />
+                {touched.firstName && errors.firstName ? (
+                  <div className="error">{errors.firstName}</div>
+                ) : null}
+              </div>
+              <div className="form-fl-10ihzr">
+                <label htmlFor="">Last Name</label>
+                <input
+                  type="text"
+                  name="lastName"
+                  id="_10ihln"
+                  {...getFieldProps("lastName")}
+                  placeholder={user.lastname}
+                  disabled={!firstNameEditMode}
+                />
+                {touched.lastName && errors.lastName ? (
+                  <div className="error">{errors.lastName}</div>
+                ) : null}
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="form-save-btn"
+              disabled={!firstNameEditMode && !lastNameEditMode}
+            >
+              Save
+            </button>
+          </div>
+        </form>
+
+        {/*  */}
+
+        <div className="info-head-10ih">
+          <span className="_10ihqw">Email Address</span>
+          <span
+            className="_10ihec"
+            onClick={() => toggleEditMode(setEmailEditMode)}
+          >
+            edit
+          </span>
+        </div>
+        <form className="info-form-10ihfff" onSubmit={handleSubmit}>
+          <div>
+            {" "}
+            <div className="form-10ihfff-fl">
+              <div className="form-fl-10ihzl">
+                <label htmlFor="">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="_10ihfn"
+                  {...getFieldProps("email")}
+                  placeholder={user.email}
+                  disabled={!emailEditMode}
+                />
+                {touched.email && errors.email ? (
+                  <div className="error">{errors.email}</div>
+                ) : null}
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="form-save-btn"
+              disabled={!emailEditMode}
+            >
+              Save
+            </button>
+          </div>
+        </form>
+
+        {/*
+         */}
+        <div className="info-head-10ih">
+          <span className="_10ihqw">Phone Number</span>
+          <span
+            className="_10ihec"
+            onClick={() => toggleEditMode(setPhoneNoEditMode)}
+          >
+            edit
+          </span>
+        </div>
+        <form className="info-form-10ihfff" onSubmit={handleSubmit}>
+          <div>
+            {" "}
+            <div className="form-10ihfff-fl">
+              <div className="form-fl-10ihzl">
+                <label htmlFor="">Phone no.</label>
+                <input
+                  type="number"
+                  name="phoneNo"
+                  id="_10ihfn"
+                  {...getFieldProps("phoneNo")}
+                  placeholder={user.phone ? user.phone : "+91-1234567890"}
+                  disabled={!phoneNoEditMode}
+                />
+                {formik.touched.phoneNo && errors.phoneNo ? (
+                  <div className="error">{formik.errors.phoneNo}</div>
+                ) : null}
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="form-save-btn"
+              disabled={!phoneNoEditMode}
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileInfo;
---
+++ .history/client/src/helper/helper_20241212155345.js
@@ -0,0 +1,378 @@
+import axios from 'axios';
+import { jwtDecode } from "jwt-decode";
+import UserModel from '../../../server/model/User.model';
+
+axios.defaults.baseURL = "http://localhost:8080";
+
+// ** Make API Requests **
+
+// ** to get the user from token **
+
+export async function getUsername() {
+    const token = localStorage.getItem('token')
+    if(!token) return Promise.reject("Cannot find token");
+
+    let decode = jwtDecode(token);
+    return decode;
+}
+
+// "" google authentication ""
+export async function signupWithGoogle(){
+  window.location.href = "http://localhost:8080/v1/api/auth/google";
+  // const result = await axios.get('/v1/api/auth/google');
+  // console.log("result", result);
+  signupWithGoogleCallback()
+}
+export async function signupWithGoogleCallback() {
+  // window.location.href = "http://localhost:8080/v1/api/auth/google";
+  const result = await axios.get('/v1/api/auth/google/callback');
+  console.log("result", result);
+}
+// ** auhenticate function **
+export async function authenticate(email) {
+    try {
+        const data = await axios.post('/v1/api/authenticate', {email});
+        const user = await axios.get(`/v1/api/user/email/${email}`);
+        console.log(data.data, user.data);
+        data.data = user.data;
+        return data;
+    } catch (error) {
+        return (error);
+    }
+};
+
+// export async function googleCallbacck() {
+  // fetch("/v1/api/auth/google/callback")
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     // Store user data in local storage
+  //     localStorage.setItem("userProfile", JSON.stringify(data.profile));
+  //     // Redirect to the main app
+  //     window.location.href = "/"; // Replace '/' with your main app route
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error fetching user profile:", error);
+  //   });
+// }
+// ** get the user details **
+export async function getUserDetail({ username }) {
+
+  try {
+    const { data } = await axios.get(`/v1/api/user/${username}`);
+    // console.log("client helper:", data);
+    return { data };
+  } catch (error) {
+    return { error: "Password doesn't match...!" };
+  }
+}
+
+// ** register user function **
+export async function registerUser({credential}){
+    try{
+        console.log("Received credential:", credential);
+        const {data: {msg, userId}, status} = await axios.post(`/v1/api/register`, credential);
+
+        // console.log("data" ,msg, userId);
+        
+        let {fullname, email} = credential;
+        // ** send email **
+        // if(status === 201){
+        //     await axios.post(`/v1/api/registerMail`, {fullname, useremail:email, text:msg})
+        // }
+        return {msg: msg, userId: userId};
+    }catch(error){
+        console.error(
+          "Error in registerUser:",
+          error.response ? error.response.data : error.message
+        );
+        return Promise.reject(error.response ?  error.response.data : error.message);
+
+    }
+}
+
+// ** login function **
+export async function loginUser({ credential }) {
+    try {
+        // console.log("Received credential:", credential);
+        const  {data}  = await axios.post(`/v1/api/login`, credential);
+        console.log("ResponseData: ", data);
+        return data;
+    } catch (error) {
+        console.error(error);
+        throw new Error("Password doesn't match...!");
+    }
+};
+
+
+
+// ** update user function **
+export async function updateUser(response){
+    try{ 
+        const token = await localStorage.getItem('token');
+        const data = await axios.put(`/v1/api/updateuser`, response, {heaader: {"Authorization": `Bearer ${token}`}});
+
+        return Promise.resolve({data})
+    }
+    catch(error){
+        return Promise.reject({error: "Couldn't Update Profile...!"})
+    }
+}
+
+
+// "" recovery email to update password ""
+export async function verifyEmailAndSendOTP(email) {
+  try {
+    if(!email){
+      return {msg: "Invalid email address"};
+    }
+    const response = await axios.post(`/v1/api/recovery`, {email});
+    // console.log("res", response);
+    return  response.data;
+
+
+  } catch (error) {
+    
+  }
+}
+
+
+// ** generate OTP **
+export async function generateOTP(email, fullname){
+    try{
+        const {data: {code}, status} =  await axios.get(`/v1/api/generate-Otp`, {params: {email}});
+        console.log("code : ", code, status);
+        // send mail with otp
+        if(status === 201){
+
+            // let {data: {email}} = await getUserDetail({fullname});
+
+            let text = `Your Password Recovery OTP is ${code}. Verify and Recover your Password`;
+            let subject = "Password Recovery OTP";
+            await axios.post(`/v1/api/registerMail`, {username: fullname, useremail: email, text, subject})
+            return Promise.resolve(code);
+        }
+        else {
+            return Promise.reject(
+              new Error(`Failed to generate OTP. Status: ${status}`)
+            );
+        }
+    }
+    catch(error){
+        console.error("Error in generateOTP:", error);
+        return Promise.reject({error});
+    }
+}
+
+// ** VerifyOtp **
+export async function verifyOtp(values){
+    try{
+      console.log("vv", values);
+      
+        const response = await axios.post(`/v1/api/verify-Otp`, values);
+        console.log(response);
+        return response.data;
+    }catch(error){
+        return Promise.reject(error);
+    }
+}
+
+// ** reset password **
+export async function resetPassword(values){
+    console.log(values);
+    try{
+      const response = await axios.put('v1/api/resetPassword', values);
+      console.log(response);
+      return response.data;
+    }
+    catch(error){
+        return Promise.reject({error});
+    }
+}
+// Update the profile info
+export async function updateProfileInfo(values, user) {
+  try {
+    console.log("values updated", user, values);
+    const response = await axios.put(`v1/api/updateUserProfile`, );
+
+    return {
+      message: "Profile information updated successfully",
+      data: response.data,
+    };
+  } catch (error) {
+    return Promise.reject({ error });
+  }
+}
+
+// Update the profile info
+export async function addAddress(formData, mode) {
+  try {
+    // Fetch the user ID asynchronously
+    // console.log(formData);
+    const userIdInfo = await getUsername();
+
+    // Once the promise is fulfilled, extract the userId
+    const userId = userIdInfo.userId;
+    console.log(userId);
+
+    // Make the PUT request with the user ID
+    const response = await axios.post(`v1/api/newAddress`, {formData, userId ,mode});
+
+    return {
+      message: "Address information Added successfully",
+      data: response.data,
+    };
+  } catch (error) {
+    return Promise.reject({ error });
+  }
+}
+
+export async function fetchAddress(){
+    try {
+      // Fetch the user ID asynchronously
+      // console.log(formData);
+      const userIdInfo = await getUsername();
+
+      // Once the promise is fulfilled, extract the userId
+      const userId = userIdInfo.userId;
+      const response = await axios.get(`v1/api/savedaddress?userId=${userId}`);
+    //   console.log("response : ", response);
+      return response;
+    } catch (error) {
+      console.error("Error fetching addresses:", error);
+    }
+}
+export async function deleteSavedAddress(addressId) {
+  try {
+    // Fetch the user ID asynchronously
+    // console.log(formData);
+    const userIdInfo = await getUsername();
+
+    // Once the promise is fulfilled, extract the userId
+    const userId = userIdInfo.userId;
+    const response = await axios.delete(`v1/api/deleteSavedAddress`, {
+      data: {
+        userId: userId,
+        addressId: addressId,
+      },
+    });
+      // console.log("response : ", response);
+    return response;
+  } catch (error) {
+    console.error("Error deletion address:", error);
+  }
+};
+
+export async function addToWatchlistApi(productId) {
+  try {
+    // Fetch the user ID asynchronously
+    // console.log(formData);
+    const userIdInfo = await getUsername();
+
+    // Once the promise is fulfilled, extract the userId
+    const userId = userIdInfo.userId;
+    console.log("user", userId, productId);
+    const response = await axios.post(`v1/api/addTowatchlist`, {
+        productId,
+        userId,
+    });
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export async function fetchWatchlistItem(){
+  try {
+    // Fetch the user ID asynchronously
+    // console.log(formData);
+    const userIdInfo = await getUsername();
+
+    // Once the promise is fulfilled, extract the userId
+    const userId = userIdInfo.userId;
+    const response = await axios.get(`v1/api/getWatchlist?userId=${userId}`);
+      console.log("response : ", response);
+    return response;
+  } catch (error) {
+    console.error("Error fetching addresses:", error);
+  }
+}
+export async function removeFromWatchlistApi(productId) {
+  try {
+    // Fetch the user ID asynchronously
+    const userIdInfo = await getUsername();
+
+    // Once the promise is fulfilled, extract the userId
+    const userId = userIdInfo.userId;
+
+    const response = await axios.delete(`v1/api/users/${userId}/watchlist/${productId}`);
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    // Throw the error with the error message from the response data
+    throw new Error(`Error: ${error.response.data.message}`);
+  }
+};
+
+
+export async function addPanCard(panData) {
+  try {
+    // Fetch the user ID asynchronously
+    // console.log(formData);
+    const userIdInfo = await getUsername();
+
+    // Once the promise is fulfilled, extract the userId
+    const userId = userIdInfo.userId;
+    const response = await axios.post(`v1/api/addPANCard`,{panData, userId});
+      console.log("response : ", response);
+    return response;
+  } catch (error) {
+    console.error("Error in Uploading:", error);
+  }
+}
+
+export async function saveProductsToServer(products){
+  console.log(products);
+  try {
+    const response = await axios.post("v1/api/products", 
+      {body: products },
+    );
+    if (!response.ok) {
+      throw new Error(
+        `Failed to save products to the server. Status: ${response.status}`
+      );
+    }
+  } catch (error) {
+    console.error("Error saving products to server:", error);
+  }
+}
+
+export async function fetchProducts() {
+  try {
+    const response = await axios.get("v1/api/getProducts");
+    return response.data || []; // Ensure an array is returned
+  } catch (error) {
+    console.error("Error in Getting Product data:", error);
+    return []; // Return an empty array in case of error
+  }
+}
+
+
+export async function fetchProductsById(id) {
+  try {
+    const response = await axios.get(`v1/api/getProductsById?Id=${id}`);
+    return response;
+  } catch (error) {
+    console.error("Error in Getting Product data:", error);
+  }
+}
+
+export async function fetchSearchProducts(searchInput){
+  try{
+    const response = await axios.get(`v1/api/getProducts/search?query=${searchInput}`);
+    console.log(response);
+    return response;
+  } catch(error){
+    console.error("Error fetching products: ", error);
+  }
+}
